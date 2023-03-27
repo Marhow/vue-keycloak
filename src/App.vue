c809f2f02872bfb61577c50a08aaa4ab135769ed
Mhowe <mhowe@behaviosec.com>
2023-03-27 11:08:38 +0200

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">Cart</a></li>
+      <li><a href="#">Checkout</a></li>
+      <li><a href="#">Account</a></li>
+    </ul>
+  </nav>
+  <section>
+    <p>Hello</p>
+    <img src="#" alt="Image">
+  </section>
+  <section>
+    <p>Goodbye</p>
+    <aside>
+      <p>Hello</p>
+    </aside>
+  </section>
+  <aside>
+    <p>Hello</p>
+  </aside>
+  <article>K</article>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './style/scss/app.scss';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
+import Keycloak from 'keycloak-js'
+import { keycloakOptions } from '../keycloak'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+let keycloak = new Keycloak(keycloakOptions)
+
+keycloak.init({ onLoad: 'login-required' }).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  }
+
+  const app = createApp(App, { props: { keycloak: keycloak } })
+  app.mount('#app')
+
+  setInterval(() => {
+    keycloak.updateToken(70).then((refreshed) => {
+      if (refreshed) {
+        console.info('Token refreshed' + refreshed)
+      } else {
+        console.warn('Token not refreshed, valid for ' +
+          Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds')
+      }
+    }).catch(() => {
+      console.error('Failed to refresh token')
+    })
+  }, 6000)
+}).catch(() => {
+  console.error('Authenticated Failed')
+})
+
